a5830879d35586d2baf480285161c129ace3cbf5
Rogelio97 <34067619+Rogelio97@users.noreply.github.com>
2020-02-13 23:56:59 -0600

    Add files via upload

---
+++ Logico_PROLOG.pl
@@ -0,0 +1,28 @@
+% This buffer is for notes you don't want to save.
+% If you want to create a file, visit that file with C-x C-f,
+% then enter the text in that file's own buffer.
+% canario(pajaro).
+%
+% ***********************
+% Hechos.
+lava(fuego).
+agua(fuente).
+madera(mesa).
+
+puede(fuego,quemar).
+puede(agua,ahogar).
+puede(mesa,construir).
+
+matar(lava).
+
+%**************.
+% Reglas.
+
+deporte(elena,tenis).
+deporte(jony,football).
+deporte(tomas,baseball).
+deporte(marco,tenis).
+deporte(_,tenis).
+deporte(juan,_).
+
+
